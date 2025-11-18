917595dd27ddb2eccc42360279918ef10be54481
Symbolic <sarrm20@icloud.com>
2025-11-17 22:30:35 -0400

    Wrote a bit more lines

--- cmdLineParser.cpp
+++ cmdLineParser.cpp
@@ -1,5 +1,5 @@
 #include <cctype>
-#include <cmdLineParser.h>
+#include "cmdLineParser.h"
 using namespace std;
 
 void TCmdLineParser::printHelpMessage() {
@@ -8,4 +8,29 @@ void TCmdLineParser::printHelpMessage() {
     cout << "   -h, --help              Display this help information" << endl;
     cout << "   -r, --romFileName       Set the rom file path to be used" << endl;
     cout << "   -l, --logLevel          Set the desired log level [NONE = 0, ERROR = 1, WARN = 2, INFO = 3, DEBUG = 4]" << endl; 
+}
+
+void TCmdLineParser::setRomFileName(string new_name) {
+    this->romFileName = new_name;
+}
+
+void TCmdLineParser::setLogLevel(int logLevel) {
+    this -> logLevel = logLevel;
+}
+
+TCmdLineParser::TCmdLineParser()
+    : romFileName(""), logLevel(-1) {
+        logger = TLogger::getInstance();
+    }
+
+void TCmdLineParser::parseCmdLine(int argc, char** argv) {
+    for (int i = 1; i < argc; i++) {
+        string auxStr(argv[i]);
+
+        if(auxStr.at(0) == '-') {
+            if (auxStr == "-h" || auxStr == "--help") {
+                this -> printHelpMessage();
+            }
+        }
+    }
 }
